01f14b44d1349238c76d9af3890c360b06807bb3
Hossam <hossamhh2@gmail.com>
2022-07-10 01:42:40 +0200

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'E-Commers Products',
+        title: 'E-Commers Products mmmmmmm',
         initialRoute: 'appPage',
         routes: routes,
       ),
