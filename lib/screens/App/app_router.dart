8d1a1ce5e22b03ebf7da660fdb7d74215f35dbe7
Hossam <hossamhh2@gmail.com>
2022-07-29 03:41:39 +0200

    arrange files

--- lib/screens/App/app_router.dart
+++
@@ -1,6 +0,0 @@
-import 'package:fluent_ui/fluent_ui.dart';
-
-Map<String, Widget Function(BuildContext context)> routes = {
-  // '/app/': (context) => const Login(),
-  // '/app': (context) => const App(),
-};
